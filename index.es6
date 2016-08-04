606e0bdfe88186b187f7e900e6bcd47c3e60347a
kyleaedwards <edwards.kyle.a@gmail.com>
2016-08-04 11:32:06 -0400

    split es6 file and update version number to 0.3.0

---
+++ index.es6
@@ -0,0 +1,46 @@
+"use strict"
+
+/**
+ * AutoQ
+ * Automatic, dynamic callback queue.
+ *
+ * @version 0.3.0
+ * @author Kyle Edwards <edwards.kyle.a@gmail.com>
+ * @license MIT
+ */
+
+/**
+ * WeakMap to privately store Promise objects for each
+ * instantiation of AutoQ.
+ */
+const promises = new WeakMap()
+const onErrors = new WeakMap()
+
+/**
+ * AutoQ class
+ */
+module.exports = class AutoQ {
+
+    constructor(onError) {
+        promises.set(this, new Promise(cb => cb(null)))
+        onErrors.set(this, onError instanceof Function ? onError : () => {})
+    }
+
+    /**
+     * Adds callbacks to queue.
+     * Supply functions with the signiture:
+     *   function (argument, callback) {}.
+     * @param next
+     * @return void
+     */
+    add(next) {
+        promises.set(
+            this,
+            promises
+                .get(this)
+                .then(arg => new Promise(cb => next(arg, cb)))
+                .catch(onErrors.get(this))
+        )
+    }
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+"use strict"
+
 /**
  * AutoQ
  * Automatic, dynamic callback queue.
  *
- * @version 0.2.2
+ * @version 0.3.0
  * @author Kyle Edwards <edwards.kyle.a@gmail.com>
  * @license MIT
  */
@@ -12,37 +14,44 @@
  * instantiation of AutoQ.
  */
 var promises = new WeakMap()
+var onErrors = new WeakMap()
 
 /**
  * AutoQ class
  */
-module.exports = class AutoQ {
-
-    /**
-     * Adds callbacks to queue.
-     * Supply functions with the signiture:
-     *   function (argument, callback) {}.
-     * If a failure callback is not supplied, success
-     * will be called under all circumstances.
-     *
-     * @param next
-     * @return AutoQ instance
-     */
-    add(next) {
-        var currentPromise = promises.get(this)
-        if (currentPromise) {
-            currentPromise = currentPromise.then(arg => new Promise(curry(next, arg)))
-        } else {
-            currentPromise = new Promise(curry(next, null))
-        }
-        promises.set(this, currentPromise)
-        return this
+function AutoQ(onError) {
+    promises.set(
+        this,
+        new Promise(function (cb) {
+            return cb(null)
+        })
+    )
+    if (onError instanceof Function) {
+        onErrors.set(this, onError)
+    } else {
+        onErrors.set(this, function () {})
     }
-
 }
 
 /**
- * Helper function to curry the supplied argument in order
- * to resolve the supplied handler to fit the Promise spec.
+ * Adds callbacks to queue.
+ * Supply functions with the signiture:
+ *   function (argument, callback) {}.
+ * @param next
+ * @return void
  */
-function curry(f, arg) { return res => f(arg, res) }
+AutoQ.prototype.add = function add(next) {
+    promises.set(
+        this,
+        promises
+            .get(this)
+            .then(function (arg) {
+                return new Promise(function (cb) {
+                    return next(arg, cb)
+                })
+            })
+            .catch(onErrors.get(this))
+    )
+}
+
+module.exports = AutoQ
